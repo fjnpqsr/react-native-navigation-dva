5fc8e24397b3a8f23fc44677216b426a8bd02297
Administrator <345239925@qq.com>
2019-04-27 18:41:58 +0800

    compatible dva-core & react-navigation & react-native

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import './app/index'
 
-AppRegistry.registerComponent(appName, () => App);
+console.disableYellowBox = true
