3be5e47e92ec835b34479d058f6a2cc8521408c3
Administrator <345239925@qq.com>
2019-05-07 01:44:40 +0800

    page onFocus system

--- app/components/Page/index.js
+++ app/components/Page/index.js
@@ -1,12 +1,29 @@
 import React from 'react'
+import { withNavigationFocus } from 'react-navigation'
 import { View } from 'react-native'
 import appConfig from '../../../app.json'
 
 class Page extends React.Component {
+  static getDerivedStateFromProps (nextProps, prevState) {
+    const { onFocus, forceFocus } = this.props
+
+    const nextFocused = nextProps.isFocused // 获取更新后页面是不是活动页
+    const prevFocused = prevState.isFocused // 获取更新前页面是不是活动页
+    const isFocusChange = nextFocused === prevFocused // 更新前的isFocused !== 更新后的isFocused ==> 页面进行了切换
+
+    let doFocus = prevFocused && isFocusChange // doFocus默认情况下处理的是: 页面切换且上一个页面是激活状态下的场景, 用于处理 A->B B->A场景
+    if (forceFocus) {
+      doFocus = true // 如果存在forceFocus则在进行页面切换的时候也会执行 onFocus, 用于处理 A->B-> ...->A第二次A页面没有刷新的情况
+    }
+    if (doFocus) {
+      onFocus()
+    }
+  }
   constructor (props) {
     super(props)
     this.state = {}
   }
+
   render () {
     const { style, ...restProps } = this.props
     const defaultStyle = { flex: 1, backgroundColor: appConfig.pageColor }
@@ -21,4 +38,4 @@ class Page extends React.Component {
   }
 }
 
-export default Page
+export default withNavigationFocus(Page)
