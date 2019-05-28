db89b82160da6b2e64abdd433d70c036c668917a
qiushaorong <345239925@qq.com>
2019-05-28 16:09:33 +0800

    add react-native-audio package

--- android/app/src/main/java/com/rnframe/MainApplication.java
+++ android/app/src/main/java/com/rnframe/MainApplication.java
@@ -3,6 +3,7 @@ package com.rnframe;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnim.rn.audio.ReactNativeAudioPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.github.yamill.orientation.OrientationPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
+            new ReactNativeAudioPackage(),
             new LinearGradientPackage(),
             new OrientationPackage(),
             new VectorIconsPackage(),
